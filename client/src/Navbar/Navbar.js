734935217e090a96cf45cac5dcecb3fd9aea79d0
70y-ali <sasulark@gmail.com>
2022-06-29 18:32:32 +0900

    add style

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return(
         <nav className="navbar">
-            <h1>Joy blog</h1>
+            <h1>JOY</h1>
             <div className="links"></div>
-            <Link to="/">Home</Link>
+            <Link to="/">HOME</Link>
+            <Link to="/us">US</Link>
+            <Link to="/futures">FUTURES</Link>
+            <Link to="/contact">CONTACT</Link>
             {/* style{{}} it is dynamic programming js */}
             <Link to="/create" style={{
                 color: "white",
                 backgroundColor: "#f1356d",
                 borderRadius: "8px"
             }}>New Blog</Link>
+            
         </nav>
     );
 }
